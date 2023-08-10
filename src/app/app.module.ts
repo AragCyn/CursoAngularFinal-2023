9d2550d9d101bdeaf7036bc897ccace23c471945
Cynthia Aragón <aragcyn@gmail.com>
2023-08-09 21:38:24 -0300

    Version 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { IntroComponent } from './intro/intro.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { WhyComponent } from './why/why.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { TeamComponent } from './team/team.component';
+import { ClientsComponent } from './clients/clients.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IntroComponent,
+    AboutComponent,
+    ServicesComponent,
+    WhyComponent,
+    PortfolioComponent,
+    TeamComponent,
+    ClientsComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
